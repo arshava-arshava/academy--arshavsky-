99c06c2a2235aef20c92f487d78227ef25550e2f
Alexandr Arshavsky <a.arshavsky@itransition.com>
2020-06-29 20:06:31 +0300

    first commit

---
+++ src/by/academy/classwork/Main.java
@@ -0,0 +1,17 @@
+package by.academy.classwork;
+
+public class Main {
+    public static void main(String ... args) {
+        int myAge = 24;
+        long passportNumber = 1234567890L;
+        char yourMark = 'A';
+        double averageMark = 4.12;
+        int test =  512;
+        System.out.println("your exam results: ");
+        System.out.println("your age is: " + myAge + ", passport number: " + passportNumber);
+        System.out.println("you got " + yourMark + ", average mark is " + averageMark);
+    }
+}
+
+
+
