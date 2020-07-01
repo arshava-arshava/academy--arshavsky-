e1750a8c1608e78ad8ee8d615691c7a2159da538
Alexandr Arshavsky <a.arshavsky@itransition.com>
2020-07-01 09:57:50 +0300

    Task2

---
+++ src/task2.java
@@ -0,0 +1,11 @@
+public class task2 {
+    public static void main (String[] args){
+       int a = 2_123_000;
+       int b = 100;
+       int c = 234;
+       int d = a / b * c;
+       System.out.println(d);
+
+
+    }
+}
