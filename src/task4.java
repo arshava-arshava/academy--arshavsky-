41106aca4d9b5a726dcdb35bb008767421014f44
Alexandr Arshavsky <a.arshavsky@itransition.com>
2020-07-01 10:21:47 +0300

    Task4

---
+++ src/task4.java
@@ -0,0 +1,10 @@
+public class task4 {
+    public static void main(String[] args){
+        int a = 3;
+        if(a%2 == 0){
+            System.out.println("Число является четным");
+        }else {
+            System.out.println("Число является нечетным");
+        }
+    }
+}
