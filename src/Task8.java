e682448e3708eac51d438f03737396b73cec11be
Alexandr Arshavsky <a.arshavsky@itransition.com>
2020-07-01 20:47:17 +0300

    Task8

---
+++ src/Task8.java
@@ -0,0 +1,8 @@
+public class Task8 {
+    public static void main(String[] args){
+        for(int i = 10; i<=20; i++){
+            int b = i*i;
+            System.out.println(b);
+        }
+    }
+}
