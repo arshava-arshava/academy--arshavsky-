4dd8519a8bdd804b0c76a41a833a9c3c47e36387
Alexandr Arshavsky <a.arshavsky@itransition.com>
2020-07-05 11:24:17 +0300

    Task20(Variety Converter)

---
+++ src/Task20.java
@@ -0,0 +1,17 @@
+public class Task20 {
+    public static void main(String[] args){
+        for(int i = 1;i <= 10; i++){
+            double sagen = i*2.1366;
+            float dume = i*2.5f;
+            double foot = i*0.3048;
+            double drahma = i*3.7325;
+            float ounce = i*29.86f;
+            double pound = i*0.40951;
+            double arshin = i*0.7112;
+            double doldiez = i*4.2657;
+            double inch = i*25.3995;
+            System.out.println();//Please, insert any variety you want.
+        }
+
+    }
+}
