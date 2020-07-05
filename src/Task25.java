fd34ed1d32e5589531ba6926ce8d2b68830ad87c
Alexandr Arshavsky <a.arshavsky@itransition.com>
2020-07-05 12:24:47 +0300

    Task25 Classwork #11

---
+++ src/Task25.java
@@ -0,0 +1,9 @@
+public class Task25 {
+    public static void main(String[] args){
+        for(int i=10; i<=99; i++){
+            if(i%4==0 && i%6!=0){
+                System.out.println(i);
+            }
+        }
+    }
+}
