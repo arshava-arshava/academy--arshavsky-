18aa2553cd294682244527cd967d2b76ffa519d7
Alexandr Arshavsky <a.arshavsky@itransition.com>
2020-07-07 20:26:23 +0300

    Task1 Array

---
+++ src/by/academy/classwork/Task1Array.java
@@ -0,0 +1,19 @@
+package by.academy.classwork;
+
+import java.util.Arrays;
+
+public class Task1Array {
+    public static void main(String[] args){
+        int[] num = new int[9];
+        for (int i = 2; i<=20; i=i+2){
+            System.out.print(i +" ");
+if(i==20){
+    System.out.println();
+}
+        }
+for(int i = 2; i<=20;i=i+2){
+    System.out.println(i);
+}
+        }
+    }
+
